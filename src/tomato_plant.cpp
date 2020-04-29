b213f98fb704e9d5d31ae027be6731bf7014b63e
victorbjo <47253754+victorbjo@users.noreply.github.com>
2020-04-29 20:08:54 +0200

    Fixed various bugs
    Now plant will grow bottom up, and will keep growing after a harvest :)

--- src/tomato_plant.cpp
+++ src/tomato_plant.cpp
@@ -2,54 +2,36 @@
 
 void tomatoPlant::grow(int days, int fertilizer)
 {
-    age = age +1;
+    age++;
     if (hight < 100) {
     hight = hight + age * growth_rate;
     }
 
     fruit_growth = fruit_growth +1;
-    if (fruit_growth > 30)
-    {
-        if (hight >= 100)
-        {
-            num_tomatoes = 5;
-        }
-        else if (hight > 80)
-        {
-            num_tomatoes =4;
-        }
-            else if (hight > 60)
-        {
-            num_tomatoes =3;
-        }
-            else if (hight > 40)
-        {
-            num_tomatoes =2;
-        }
-            else if (hight > 20)
-        {
-            num_tomatoes =1;
-        }
-    }
-    else
-    {
-        num_tomatoes = 0;
-    }
+
     
+        if (fruit_growth > 30)
+        {
+            num_tomatoes = (fruit_growth-30)/3;
+            num_tomatoes > 5 ? num_tomatoes = 5: num_tomatoes = num_tomatoes;
+        }
 }
 void tomatoPlant::harvestFruits()
 {
-    fruit_growth = 0;
+    if (num_tomatoes > 0){
+        fruit_growth = 31;
+    }
 }
-
+void tomatoPlant::grow_new_fruits()
+{
+    fruit_growth = 25;
+}
 void tomatoPlant::growthRate(int water)
 {
+    growth_rate = 2;
     if (water < 1)
     {
         growth_rate = 1;
     }
-    else
-    {
-         growth_rate = 2;
-    }
+
 }
