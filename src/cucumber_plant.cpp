502a93d2d3ebb458be2cf8e3255dd452c5e08a68
victorbjo <47253754+victorbjo@users.noreply.github.com>
2020-05-01 10:14:36 +0200

    Update cucumber_plant.cpp

--- src/cucumber_plant.cpp
+++ src/cucumber_plant.cpp
@@ -2,36 +2,36 @@
 #include <stdlib.h>
 void CucumberPlant::grow(int days, int fertilizer)
 {
-        if (hight < 200) 
-        {
-            hight = hight + days * growth_rate;
-        }
-
-    fruit_growth = fruit_growth +1;
+    if (hight < 200) 
+    {
+        hight = hight + days * growth_rate;
+    }
+    if (growth_rate > 0)
+    {
+        fruit_growth = fruit_growth +1;
+    }
     if (fruit_growth > 32)
     {
         num_cucumbers = (fruit_growth-32)/4;
         num_cucumbers > 5 ? num_cucumbers = 5: num_cucumbers = num_cucumbers;
     }
-    
 }
 
 void CucumberPlant::harvestFruits()
 {
-    if (num_cucumbers > 0){
-    fruit_growth = 33;
+    if (num_cucumbers > 0)
+    {
+        fruit_growth = 33;
     }
-    
 }
 
 void CucumberPlant::growthRate(int water)
-{   float growth_modifier = rand() % 120 + 80;
+{   
+    float growth_modifier = rand() % 120 + 80;
     growth_modifier = growth_modifier/100;
     growth_rate = 4*growth_modifier;
     if (water < 1)
     {
-        growth_rate = 1*growth_modifier;
-    }
-
-    
+        growth_rate = -0.5*growth_modifier;
+    }    
 }
