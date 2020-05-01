0c70efed5b25d8499a9b037dcd6b9a22fd30ce1f
victorbjo <47253754+victorbjo@users.noreply.github.com>
2020-05-01 10:16:22 +0200

    Update tomato_plant.h

--- include/tomato_plant.h
+++ include/tomato_plant.h
@@ -11,10 +11,14 @@ public:
     int getType() override {return type; };
     void grow(int days, int fertilizer) override;
     void growthRate(int water) override;
-    int getNumFruits() override {return num_tomatoes; };
+    int get_num_fruits() override {return num_tomatoes; };
     void harvestFruits() override;
-    
+    int get_days_without_water(){return days_without_water;};
+    void set_days_without_water(){days_without_water++;};
+    void reset_days_without_water(){days_without_water=0;};
+    void kill(){alive = false;};
 private:
+    int days_without_water;
     int hight{0};
     int age = 0;
     float maxHight{100};
@@ -22,4 +26,5 @@ private:
     float growth_rate{2.0};
     int type = 1;
     int fruit_growth{1};
+    bool alive = true;
 };
