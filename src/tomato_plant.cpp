b4cd4b8fa2ca42f0f4c4dae13bc140207551bd97
victorbjo <47253754+victorbjo@users.noreply.github.com>
2020-05-01 10:13:59 +0200

    Update tomato_plant.cpp

--- src/tomato_plant.cpp
+++ src/tomato_plant.cpp
@@ -3,22 +3,25 @@
 void tomatoPlant::grow(int days, int fertilizer)
 {
     age++;
-    if (hight < 100) {
-    hight = hight + age * growth_rate;
+    if (hight < 100) 
+    {
+        hight = hight + age * growth_rate;
     }
-
-    fruit_growth = fruit_growth +1;
-
-    
+    if (growth_rate > 0)
+    {
+        fruit_growth = fruit_growth +1; 
+    }
         if (fruit_growth > 30)
         {
             num_tomatoes = (fruit_growth-30)/3;
             num_tomatoes > 5 ? num_tomatoes = 5: num_tomatoes = num_tomatoes;
         }
 }
+
 void tomatoPlant::harvestFruits()
 {
-    if (num_tomatoes > 0){
+    if (num_tomatoes > 0)
+    {
         fruit_growth = 31;
     }
 }
@@ -30,7 +33,6 @@ void tomatoPlant::growthRate(int water)
     growth_rate = 2*growth_modifier;
     if (water < 1)
     {
-        growth_rate = 1*growth_modifier;
+        growth_rate = -0.5*growth_modifier;
     }
-
 }
