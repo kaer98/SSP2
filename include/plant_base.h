09146518051bac2ca9162e88f19fc5301a7b8cff
victorbjo <47253754+victorbjo@users.noreply.github.com>
2020-05-01 10:17:36 +0200

    Update plant_base.h

--- include/plant_base.h
+++ include/plant_base.h
@@ -8,8 +8,10 @@ class plantBase
         virtual int getType() = 0;
         virtual void grow(int days, int fertilizer) = 0;
         virtual void growthRate(int water) = 0;
-        virtual int getNumFruits() = 0;
+        virtual int get_num_fruits() = 0;
         virtual void harvestFruits() = 0;
+        
     private:
         int type;
+        
 };
